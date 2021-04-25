e46447bd6dd4208a43a3066e4a3c29aeeae391ff
moondog <21975249+rodionus@users.noreply.github.com>
2021-04-25 19:51:33 +0400

    repo init

---
+++ Assets/Scripts/player/player_controller.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    // общие переменные
+    public bool is_grounded = false;
+
+    // переменные реализации передвижения
+    public float player_speed = 5;
+    public float mouse_speed = 1;
+    public float jump_forse = 3;
+    private Rigidbody rb;
+
+    // переменные реализации поворота головы
+    public float sensitivity = 100;
+    float xRotation = 0;
+    public Transform cam;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    void FixedUpdate()
+    {
+        MovementLogic();
+        JumpLogic();
+    }
+
+    private void JumpLogic(){
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (is_grounded)
+            {
+                rb.AddForce(Vector3.up * jump_forse);
+            }
+        }
+    }
+
+    private void MovementLogic()
+    {
+        float move_horizontal = Input.GetAxis("Horizontal");
+        float move_vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement_vector = new Vector3(move_horizontal, 0, move_vertical);
+
+        transform.Translate(movement_vector * player_speed * Time.fixedDeltaTime);
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);// ограничения на осмотр вверх и вниз
+
+        cam.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        IsGroundedUpate(collision, true);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        IsGroundedUpate(collision, false);
+    }
+
+    private void IsGroundedUpate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            is_grounded = value;
+        }
+    }
+}
---
+++ Assets/Scripts/weapons/behawour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class behawour : MonoBehaviour
+{
+    public Animator animator;
+    public Animator cam_animator;
+
+    public Rigidbody player_rb;
+    public float impact_forse;
+
+    public Transform pistol;
+    public bool is_pos1;
+    private Quaternion pos1;
+    private Quaternion pos2;
+
+    public void Start()
+    {
+        pos1 = new Quaternion(0, 0, 0, 0);
+        pos2 = new Quaternion(0, 0, 60, 0);
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.Play("shoot");
+            cam_animator.Play("cam_shoot");
+            player_rb.AddForce(transform.up * -impact_forse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (is_pos1 == true)
+            {
+                pistol.rotation = pos2;
+                is_pos1 = !is_pos1;
+            }
+            else
+            {
+                pistol.rotation = pos1;
+                is_pos1 = !is_pos1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.Play("reload");
+        }
+    }
+}
